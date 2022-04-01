393bf8bcb926b4a8908e815d0f18222c1dde6d16
Dhruva Naik <90971708+BoltyCodes@users.noreply.github.com>
2022-04-01 19:30:27 +0300

    Create Employee.sql

---
+++ Employee.sql
@@ -0,0 +1,19 @@
+/*CREATE TABLE Employe(emp_id int, name char(20), role varchar(100) )*/
+
+/*INSERT into Employe(emp_id, name, role)values(32, "Jhonny", "Manager")*/
+
+/*insert into Employe(emp_id, name, role)values(2, "Jhon", "Manager")*/
+/*insert into Employe(emp_id, name, role)values(96, "Andrew", "Barista")*/
+/*insert into Employe(emp_id, name, role)values(89, "Dhruva", "Waiter")*/
+/*insert into Employe(emp_id, name, role)values(69, "Lin Ho", "employe")*/
+
+/*sELECT * FROM Employe*/
+/*SELECT name from Employe*/
+/*SELECT name,role from Employe*/
+
+/*DELETE from Employe*/
+/*drop table Employe*/
+
+
+
+
